94fb632c8f554358be0d0c2df7e2acf342d88284
Keith Leffler <keithleffler68@gmail.com>
2018-07-21 10:43:22 -0700

    Define basic program and matching unit test structure.  Define a default input file.  Define a function to return file locations.  These are fixed by the assignment, but should be easily changable to handle different values. All unit tests are expected to fail.

---
+++ programming/rev_sort/rev_sort.py
@@ -0,0 +1,119 @@
+
+import logging
+
+'''
+'''
+
+logging.basicConfig(level=logging.INFO)
+
+
+class codeException(Exception):
+    # TODO: Add docstring.
+    # TODO: Add boilerplate code-assertion message
+    pass
+
+
+class inputException(Exception):
+    # TODO: And  docstring
+    # TODO: Add boilerplate input-problem message
+    pass
+
+def check_input_file(filename):
+    # TODO: Write a function to check existence of input file.
+    #  Raise an exception if not OK
+
+    return True
+
+def check_output_path(filename):
+    # TODO: Write a function to check that output is possible.
+    # path exists,
+    # permissions are OK to write to this locaion,
+    # file can be over-written if it exists.
+    # Raise exception if not OK
+
+    return True
+
+def get_args():
+    # TODO: Add docstring
+    try:
+
+        args = {'infile':'./input.csv',
+                'outfile':'./output.csv'}
+
+        check_input_file(args['infile'])
+        check_output_path(args['outfile'])
+
+        return args
+
+    except Exception as e:
+        raise codeException('Code assertion: method get_args: %s' % e)
+
+
+def get_input(infile):
+    '''
+    Read the first line of the input file, and return as a string.
+    :return:
+    '''
+
+    # TODO - Add check that file is only one line.
+    # Fail if this isn't so.
+
+    try:
+        assert False
+    except Exception as e:
+        raise codeException('Code assertion, method get_input: %s'%e)
+
+
+def rev_sort(s):
+    '''
+    Sort elements of a comma-separated string into reverse order.
+    :param s: A comma-separated string
+    :return: A CSV string, with the elements of the input CSV sorted in descending order
+    '''
+
+    def clean_up(s):  # TODO: expand the set of problematic substrings.  Remove leading spaces.  Preserve internal space.
+        pass
+
+
+    def do_sort(s):
+        pass
+
+    def post_process(s):
+        _s = '%s\n' % s
+        return _s
+
+    try:
+        assert False
+
+    except Exception as e:
+        raise codeException('Code assertion: method rev_sort: %s' % e)
+
+
+def write_output(outfile, s):
+    '''
+    Write a string to a text file
+    :param outfile:
+    :param s:
+    :return:
+    '''
+    try:
+        assert False
+
+    except Exception as e:
+        raise codeException('Code assertion: method write_output: write_output: %s' % e)
+
+
+def main():
+    # TODO: Add docstring
+    try:
+        assert False
+
+    except codeException as c:
+        logging.debug('Code exception %s' % c)
+
+    except Exception as e:
+        logging.fatal('Unexpected program error %s' % e)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
---
+++ programming/rev_sort/tests/test_rev_sort.py
@@ -0,0 +1,33 @@
+import unittest
+from .. import rev_sort
+
+class _testCase(unittest.TestCase):
+    pass
+
+class test_check_input_file(unittest.TestCase):
+    def test_happy_path(self):
+        self.fail('Not implemented')
+
+class test_check_output_file(unittest.TestCase):
+    def test_happy_path(self):
+        self.fail('Not implemented')
+
+class test_get_args(_testCase):
+    def test_happy_path(self):
+        self.fail('Not implemented')
+
+class test_get_input(_testCase):
+    def test_happy_path(self):
+        self.fail('Not implemented')
+
+class test_rev_sort(_testCase):
+    def test_happy_path(self):
+        self.fail('Not implemented')
+
+class test_write_output(_testCase):
+    def test_happy_path(self):
+        self.fail('Not implemented')
+
+class test_main(_testCase):
+    def test_happy_path(self):
+        self.fail('Not implemented')
